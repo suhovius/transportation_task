8e0f46d81868a7b335ca5cb453dc5c66cb9668b1
Alexey Suhoviy <alexey.suhoviy@gmail.com>
2019-03-31 22:28:05 +0300

    Refactor distribution check

--- amount_distributiuon.go
+++ amount_distributiuon.go
@@ -17,32 +17,55 @@ func sumMatchCheck(entityName string, index int, actualSum, expectedSum float64)
 	return
 }
 
-func (t *Task) amountDistributionCheck() (err error) {
-	// check rows
-	for i, supply := range t.supplyList {
+func (t *Task) amountDistributionCheckFor(
+	entityName string,
+	finder func(i, j int) tableCell,
+	listsPair func() (r, c *[]tableOuterCell),
+) (err error) {
+	rowsList, colsList := listsPair()
+	for i, outerCell := range *rowsList {
 		var actualSum float64
-		for j := range t.demandList {
-			actualSum += t.tableCells[i][j].deliveryAmount
+		for j := range *colsList {
+			actualSum += finder(i, j).deliveryAmount
 		}
 
-		err = sumMatchCheck("supply", i, actualSum, supply.amount)
+		err = sumMatchCheck(entityName, i, actualSum, outerCell.amount)
 		if err != nil {
 			return err
 		}
+	}
+	return
+}
+
+func (t *Task) rowCellFinder(i, j int) tableCell {
+	return t.tableCells[i][j]
+}
+
+func (t *Task) rowListsPair() (rowsList, colsList *[]tableOuterCell) {
+	return &t.supplyList, &t.demandList
+}
+
+func (t *Task) colCellFinder(i, j int) tableCell {
+	// reverse indexes here
+	return t.tableCells[j][i]
+}
+
+func (t *Task) colListsPair() (rowsList, colsList *[]tableOuterCell) {
+	// reverse lists here
+	return &t.demandList, &t.supplyList
+}
+
+func (t *Task) amountDistributionCheck() (err error) {
+	// check rows
+	err = t.amountDistributionCheckFor("supply", t.rowCellFinder, t.rowListsPair)
+
+	if err != nil {
+		return err
 	}
 
 	// check columns
-	for i, demand := range t.demandList {
-		var actualSum float64
-		for j := range t.supplyList {
-			actualSum += t.tableCells[j][i].deliveryAmount
-		}
+	err = t.amountDistributionCheckFor("demand", t.colCellFinder, t.colListsPair)
 
-		err = sumMatchCheck("demand", i, actualSum, demand.amount)
-		if err != nil {
-			return err
-		}
-	}
 	// no any errors here
 	return
 }
