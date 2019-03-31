16f954cb9bcda30a40befdafb9260f20b0ae6118
Alexey Suhoviy <alexey.suhoviy@gmail.com>
2019-03-31 20:01:22 +0300

    Add print formatting

--- main.go
+++ main.go
@@ -22,27 +22,30 @@ func testJSONData() []byte {
 	if err != nil {
 		fmt.Println("Marshal error:", err)
 	}
-	fmt.Println("JSON:")
-	fmt.Println(string(jsonBlob))
 	return jsonBlob
 }
 
 func main() {
-	params, err := ParseParams(testJSONData())
+	printLine()
+	jsonBlob := testJSONData()
+	fmt.Println("Received JSON:")
+	fmt.Println(string(jsonBlob))
+	params, err := ParseParams(jsonBlob)
 
 	if err != nil {
 		fmt.Println("ParseParams error:", err)
 		return
 	}
 
+	printLine()
+
 	// TODO: Validate parameters cost table dimensions and supply demand list dimensions
 	// TODO: Validate parameters
 
 	task := buildTaskFromParams(params)
-
 	fmt.Println("Initial State")
 	task.print()
-	fmt.Println()
+	printLine()
 
 	kind := task.performBalancing()
 	switch kind {
@@ -54,35 +57,37 @@ func main() {
 		fmt.Println("Balancing: Add fake supply")
 	}
 	task.print()
-	fmt.Println()
+	printLine()
 
 	// And might need to have added small floating point numbers here (0.001) to fix this degeneracy issue
 	fmt.Println("Degeneracy Prevention: Add small amount to prevent degeneracy")
 	task.preventDegeneracy()
 	task.print()
-	fmt.Println()
+	printLine()
 
 	fmt.Println("Base Plan: Calculated with 'North West Corner' method")
 	task.northWestCorner()
 	task.print()
-	fmt.Printf("Delivery Cost: %f\n", task.deliveryCost())
+	fmt.Printf("\nDelivery Cost: %f", task.deliveryCost())
+	printLine()
 
-	// Transport potentials method starts here. It should be loop
+	fmt.Print("Amount distribution sum check:")
+	printLine()
 
-	// 1 Size distribution sum check
-
-	// 2 Degeneracy check
 	// http://cyclowiki.org/wiki/%D0%92%D1%8B%D1%80%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C_%D0%B2_%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%BE%D0%B9_%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B5
-
-	fmt.Print("\nDegeneracy Check:")
+	fmt.Print("Degeneracy Check:")
 	if task.isDegenerate() {
 		// TODO: Maybe return error here
-		fmt.Println(" is Degenerate!")
+		fmt.Print(" is Degenerate!")
 		return
 	}
-	fmt.Println(" is not Degenerate")
-	fmt.Println()
+	fmt.Print(" is not Degenerate")
+	printLine()
 
 	// TODO: Round numners in api response generation and return int values there
 	// https://yourbasic.org/golang/round-float-to-int/
+}
+
+func printLine() {
+	fmt.Print("\n\n=====================================================================================\n\n")
 }
