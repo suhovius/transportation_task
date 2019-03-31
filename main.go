2f46679e4a4b336216341402a03ed2f50849b68c
Alexey Suhoviy <alexey.suhoviy@gmail.com>
2019-03-31 04:59:20 +0300

    Add comments

--- main.go
+++ main.go
@@ -40,4 +40,12 @@ func main() {
 	task.print()
 	fmt.Printf("Delivery Cost: %d", task.deliveryCost())
 	fmt.Println()
+
+	// Transport potentials method starts here
+
+	// 1 Size distribution sum check
+
+	// 2 Degeneracy check
+	// http://cyclowiki.org/wiki/%D0%92%D1%8B%D1%80%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C_%D0%B2_%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%BD%D0%BE%D0%B9_%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B5
+	// And might need to have added small floating point numbers here (0.001) to fix this degeneracy issue
 }
