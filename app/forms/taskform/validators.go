4c1da3ddb1000207534d26a329898b1846180463
Alexey Suhoviy <alexey.suhoviy@gmail.com>
2019-04-10 00:03:56 +0300

    WIP taskform validations

--- app/forms/taskform/validators.go
+++ app/forms/taskform/validators.go
@@ -2,21 +2,34 @@ package taskform
 
 import (
 	"fmt"
-
-	"bitbucket.org/suhovius/transportation_task/app/forms/validate"
 )
 
-type supplyListValidator struct {
-	validate.Validator
-	params *Params
+// Validate performs Params validation
+func (p *Params) Validate() error {
+	return p.performValidations(
+		validateSupplyListSize,
+		validateDemandListSize,
+		validateCostTableRowsSizes,
+	)
 }
 
-func (v *supplyListValidator) Perform() (err error) {
-	supplyCount := len(v.params.SupplyList)
-	costRowsCount := len(v.params.CostTable)
+type validator func(p *Params) error
+
+func (p *Params) performValidations(validators ...validator) (err error) {
+	for _, v := range validators {
+		if err = v(p); err != nil {
+			break
+		}
+	}
+	return
+}
+
+func validateSupplyListSize(p *Params) (err error) {
+	supplyCount := len(p.SupplyList)
+	costRowsCount := len(p.CostTable)
 	if supplyCount != costRowsCount {
 		err = fmt.Errorf(
-			"Supply list size %d should be equal to Cost table rows count %d",
+			"Supply list size (%d) should be equal to Cost table rows count (%d)",
 			supplyCount, costRowsCount,
 		)
 	}
@@ -24,9 +37,31 @@ func (v *supplyListValidator) Perform() (err error) {
 	return
 }
 
-// Validate performs Params validation
-func (p *Params) Validate() error {
-	return validate.Init(
-		&supplyListValidator{params: p},
-	).Run()
+func validateDemandListSize(p *Params) (err error) {
+	demandCount := len(p.DemandList)
+	columnsCount := len(p.CostTable[0])
+	if demandCount != columnsCount {
+		err = fmt.Errorf(
+			"Demand list size (%d) should be equal to Cost table columns count (%d)",
+			demandCount, columnsCount,
+		)
+	}
+
+	return
+}
+
+func validateCostTableRowsSizes(p *Params) (err error) {
+	demandCount := len(p.DemandList)
+	for j, row := range p.CostTable {
+		rowSize := len(row)
+		if demandCount != rowSize {
+			err = fmt.Errorf(
+				"Cost table row [%d] size (%d) should be equal to Demand list size (%d)",
+				j, rowSize, demandCount,
+			)
+
+			break
+		}
+	}
+	return
 }
--- app/forms/validate/validator.go
+++
@@ -1,6 +0,0 @@
-package validate
-
-// Validator is an interface that should be implemented by all the validators
-type Validator interface {
-	Perform() error
-}
